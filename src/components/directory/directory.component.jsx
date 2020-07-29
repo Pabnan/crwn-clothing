fba179eaba1aa01e37031b73e4092e02ea494c64
ariel naniong <pablonaniong@gmail.com>
2020-07-29 12:55:00 +0800

    update our menu items to navigate withRouter HOC

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -10,13 +10,13 @@ export default class Directory extends React.Component {
                   title: 'hats',
                   imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
                   id: 1,
-                  linkUrl: 'shop/hats'
+                  linkUrl: 'hats'
                 },
                 {
                   title: 'jackets',
                   imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
                   id: 2,
-                  linkUrl: 'shop/jackets'
+                  linkUrl: 'jackets'
                 },
                 {
                   title: 'sneakers',
@@ -49,12 +49,8 @@ export default class Directory extends React.Component {
     
         return(
             <div className='directory-menu'>
-{this.state.sections.map(({title,imageUrl,size,id,linkUrl}) => (
-          <MenuItem key={id}
-          title={title}
-          imageUrl={imageUrl}
-          size={size}
-          />
+{this.state.sections.map(({id,...otherSectionProps}) => (
+          <MenuItem key={id} {...otherSectionProps} />
 
           ))}
                  
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -4,7 +4,7 @@ import Directory from '../../components/directory/directory.component.jsx'
 
  const HomePage = () =>(
     <div className='homepage'>
-        <Directory/>
+        <Directory  />
     </div>
 );
 export default HomePage;
